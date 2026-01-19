06fef6399e2ce7acf549bf812f19740da5dfe75e
Davincs-Xtech <davincstechproject@gmail.com>
2026-01-19 21:20:13 +0300

    Create generateToken.js

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+  if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET is required in .env');
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
+
+module.exports = generateToken;
